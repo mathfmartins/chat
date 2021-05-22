811bf7cfff427fdad7ef60825d0c211e18fbea48
matheus f <math.fmartins42@gmail.com>
2021-05-22 14:18:01 -0300

    uhul

--- lib/view/contatos.page.dart
+++ lib/view/contatos.page.dart
@@ -1,5 +1,6 @@
 import 'package:chat/firebase/contatos.firebase.dart';
 import 'package:chat/models/contato.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -34,6 +35,7 @@ class _ContatosPageState extends State<ContatosPage> {
 
   @override
   Widget build(BuildContext context) {
+    // var snapshots = Firestore.instance.collection('contatos').where('excluido', isEqualTo: false).snapshots();
     return Scaffold(
       appBar: AppBar(
         title: Text("Contatos"),
@@ -73,61 +75,119 @@ class _ContatosPageState extends State<ContatosPage> {
           child: Icon(Icons.add),
           backgroundColor: Colors.purple,
       ),
-      body: ListView.builder(
-          padding: EdgeInsets.all(10.0),
-          itemCount: contatos.length,
-          itemBuilder: (context, index) {
-            return _contactCard(context, index);
-          }
-      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('contatos').snapshots(),
+        // ignore: missing_return
+        // ignore: non_constant_identifier_names
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            switch(snapshot.connectionState) {
+                  case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator(),
+                  );
+                  default:
+                    List<DocumentSnapshot> documents =  snapshot.data.documents;
+                    return ListView.builder(
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index){
+                        return GestureDetector(
+                            child: Card(
+                              child: Padding(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Container(
+                                        width: 60.0,
+                                        height: 60.0,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          image: DecorationImage(
+                                              image: AssetImage("images/person.png"),
+                                              fit: BoxFit.cover
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(left: 10.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(documents[index].data['nome'] ?? "",
+                                              style: TextStyle(fontSize: 22.0,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Text(documents[index].data['email'] ?? "",
+                                              style: TextStyle(fontSize: 18.0),
+                                            ),
+                                            Text(documents[index].data['telefone'] ?? "",
+                                              style: TextStyle(fontSize: 18.0),
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                              ),
+                            ),
+                            onTap: (){
+                              _mostrarOpcoes(context, index);
+                            },
+                        );
+                      }, 
+                    );
+            }
+            // return _contactCard(context, snapshot)
+        },
+      )
     );
   }
 
-  Widget _contactCard(BuildContext context, int index){
-    return GestureDetector(
-      child: Card(
-        child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Row(
-              children: <Widget>[
-                Container(
-                  width: 80.0,
-                  height: 80.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: AssetImage("images/person.png"),
-                        fit: BoxFit.cover
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(contatos[index].nome ?? "",
-                        style: TextStyle(fontSize: 22.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(contatos[index].email ?? "",
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      Text(contatos[index].telefone?? "",
-                        style: TextStyle(fontSize: 18.0),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-        ),
-      ),
-      onTap: (){
-        _mostrarOpcoes(context, index);
-      },
-    );
-  }
+  // Widget _contactCard(BuildContext context, int index){
+  //   return GestureDetector(
+  //     child: Card(
+  //       child: Padding(
+  //           padding: EdgeInsets.all(10.0),
+  //           child: Row(
+  //             children: <Widget>[
+  //               Container(
+  //                 width: 80.0,
+  //                 height: 80.0,
+  //                 decoration: BoxDecoration(
+  //                   shape: BoxShape.circle,
+  //                   image: DecorationImage(
+  //                       image: AssetImage("images/person.png"),
+  //                       fit: BoxFit.cover
+  //                   ),
+  //                 ),
+  //               ),
+  //               Padding(
+  //                 padding: EdgeInsets.only(left: 10.0),
+  //                 child: Column(
+  //                   crossAxisAlignment: CrossAxisAlignment.start,
+  //                   children: <Widget>[
+  //                     Text(contatos[index].nome ?? "",
+  //                       style: TextStyle(fontSize: 22.0,
+  //                           fontWeight: FontWeight.bold),
+  //                     ),
+  //                     Text(contatos[index].email ?? "",
+  //                       style: TextStyle(fontSize: 18.0),
+  //                     ),
+  //                     Text(contatos[index].telefone?? "",
+  //                       style: TextStyle(fontSize: 18.0),
+  //                     )
+  //                   ],
+  //                 ),
+  //               )
+  //             ],
+  //           ),
+  //       ),
+  //     ),
+  //     onTap: (){
+  //       _mostrarOpcoes(context, index);
+  //     },
+  //   );
+  // }
 
   void _mostrarOpcoes(BuildContext context, int index){
     showModalBottomSheet(
